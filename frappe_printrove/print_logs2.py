695ab430a3e214e4183e70019ec7598afd37ca04
Aryan Singh <aquiveal@gmail.com>
2026-04-12 04:28:04 +0530

    refactor: remove print_logs2 utility script
    Removes the print_logs2.py file which was used to fetch and print the most recent Error Log entry from the database. This cleanup removes unused debugging code.

--- frappe_printrove/print_logs2.py
+++
@@ -1,7 +0,0 @@
-import frappe
-def execute():
-    logs = frappe.get_all('Error Log', fields=['method', 'error'], order_by='creation desc', limit=1)
-    for log in logs:
-        print('--- LOG ---')
-        print(log.method)
-        print(log.error)
